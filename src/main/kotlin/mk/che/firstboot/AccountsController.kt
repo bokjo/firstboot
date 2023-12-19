d9ced92767517ce0cbe694f3b4b0a09b35ef417e
Bojanche Stojchevski <bojanche@propel.us>
2023-12-20 00:33:02 +0100

    feat: add configuration files and env profiles

--- src/main/kotlin/mk/che/firstboot/AccountsController.kt
+++ src/main/kotlin/mk/che/firstboot/AccountsController.kt
@@ -1,16 +1,19 @@
 package mk.che.firstboot
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-data class ViewAccount(val id: Int, val name: String)
+data class ViewAccount(val id: Int, val name: String, val buildVersion: String)
 
 @RestController
 @RequestMapping("/accounts")
-class AccountsController {
-    @GetMapping("", "/a", "/b")
+class AccountsController(
+    @Value("\${che.firstboot.buildVersion}") val buildVersion: String,
+) {
+    @GetMapping()
     fun getAllAccounts(): Iterable<ViewAccount> {
-        return listOf(ViewAccount(id = 1, name = "Test 1"))
+        return listOf(ViewAccount(id = 1, name = "Test 1", buildVersion))
     }
 }
